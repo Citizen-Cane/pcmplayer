dfa8c779d7b073ed087e4893db20dac159aee0ed
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2015-05-09 17:17:01 +0200

    Fixed TimeFrom condition - the Duration class was broken because I didn't care to test it in the first place -> stupid me

--- src/pcm/model/Duration.java
+++ src/pcm/model/Duration.java
@@ -1,36 +1,40 @@
 package pcm.model;
 
 public class Duration {
-	final long time;
+    final long time;
 
-	public Duration(String duration)
-	{
-		time = parse(duration);
-	}
-	
-	public long getTime() {
-		return time;
-	}
+    public Duration(String duration) {
+        time = parse(duration);
+    }
 
-	static public boolean validate(String duration)
-	{
-		// Fixed format hh:mm"ss
-		return (duration.length() == 8 &&
-				duration.charAt(2) == ':' &&
-				duration.charAt(5) == '"');
-	}
+    /**
+     * Get the number of milliseconds of this duration
+     * 
+     * @return The duration in milli seconds
+     */
+    public long getTime() {
+        return time;
+    }
 
-	/**
-	 * Parse a PCM duration artgument into seconds starting from 1970 (to be compatibvle with java.util.Date)
-	 * @param timeFrom miliseconds of the duration
-	 * @return
-	 */
-	private long parse(String duration)
-	{
-		long hours = Integer.parseInt(duration.substring(0, 1));
-		long minutes = Integer.parseInt(duration.substring(3, 4));
-		long seconds =  Integer.parseInt(duration.substring(6, 7));
-		return (3600 * hours  + 60 * minutes  + seconds) * 1000;
-	}
+    static public boolean validate(String duration) {
+        // Fixed format hh:mm"ss
+        return (duration.length() == 8 && duration.charAt(2) == ':' && duration
+                .charAt(5) == '"');
+    }
+
+    /**
+     * Parse a PCM duration argument into seconds starting from 1970 (to be
+     * compatible with java.util.Date)
+     * 
+     * @param timeFrom
+     *            milliseconds of the duration
+     * @return
+     */
+    private long parse(String duration) {
+        long hours = Integer.parseInt(duration.substring(0, 2));
+        long minutes = Integer.parseInt(duration.substring(3, 5));
+        long seconds = Integer.parseInt(duration.substring(6, 8));
+        return (3600 * hours + 60 * minutes + seconds) * 1000;
+    }
 
 }
--- src/pcm/state/conditions/TimeFrom.java
+++ src/pcm/state/conditions/TimeFrom.java
@@ -5,39 +5,43 @@ import java.util.Date;
 import pcm.model.Duration;
 import pcm.state.Condition;
 import pcm.state.State;
+import teaselib.TeaseLib;
 
 public class TimeFrom implements Condition {
 
-	final int n;
-	final String timeFrom;
-	
-	public TimeFrom(int n, String timeFrom) {
-		this.n = n;
-		this.timeFrom = timeFrom;
-	}
+    final int n;
+    final String timeFrom;
 
-	@Override
-	public boolean isTrueFor(State state) {
-		long now = state.getTime() * 1000; // milliseconds
-		Date setTime = state.getTime(n);
-		if (setTime != null)
-		{
-			long elapsedDuration = now - setTime.getTime();
-			long requestedDuration = new Duration(timeFrom).getTime();
-			if (elapsedDuration < requestedDuration)
-			{
-				return false;
-			}
-		}
-		else
-		{
-			return false;
-		}
-		return true;
-	}
+    public TimeFrom(int n, String timeFrom) {
+        this.n = n;
+        this.timeFrom = timeFrom;
+    }
 
-	@Override
-	public String toString() {
-		return n + " " + timeFrom; 
-	}
+    @Override
+    public boolean isTrueFor(State state) {
+        long now = state.getTime() * 1000; // milliseconds
+        Date setTime = state.getTime(n);
+        if (setTime != null) {
+            long elapsedDuration = now - setTime.getTime();
+            long requestedDuration = new Duration(timeFrom).getTime();
+            TeaseLib.log("TimeFrom: setTime = " + setTime.toString()
+                    + ", duration = " + timeFrom + "(" + requestedDuration
+                    + ") , now = " + new Date(System.currentTimeMillis())
+                    + ", elapsed = " + elapsedDuration + "ms");
+            if (elapsedDuration < requestedDuration) {
+                TeaseLib.log("-> early");
+                return false;
+            }
+        } else {
+            TeaseLib.log("Warning - setTime not called on action " + n);
+            return false;
+        }
+        TeaseLib.log("-> late");
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return n + " " + timeFrom;
+    }
 }
