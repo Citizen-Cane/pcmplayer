ddb65b87655d91c942d4aefc7c1780afc5c3bf1f
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2015-05-31 20:44:11 +0200

    Fixed warning message

--- src/pcm/model/ActionDelay.java
+++ src/pcm/model/ActionDelay.java
@@ -1,30 +1,25 @@
 package pcm.model;
 
-public class ActionDelay
-{
-	public final int from;
-	public final int to;
-	public final ActionRange stop;
-	
-	public ActionDelay(int delay)
-	{
-		this.from =
-		this.to = delay;
-		this.stop = null;
-	}
+public class ActionDelay {
+    public final int from;
+    public final int to;
+    public final ActionRange stop;
 
-	public ActionDelay(int from, int to)
-	{
-		this.from = from;
-		this.to = to;
-		this.stop = null;
-	}
+    public ActionDelay(int delay) {
+        this.from = this.to = delay;
+        this.stop = null;
+    }
 
-	public ActionDelay(int from, int to, ActionRange stop)
-	{
-		this.from = from;
-		this.to = to;
-		this.stop = stop;
-	}
-	
-};
+    public ActionDelay(int from, int to) {
+        this.from = from;
+        this.to = to;
+        this.stop = null;
+    }
+
+    public ActionDelay(int from, int to, ActionRange stop) {
+        this.from = from;
+        this.to = to;
+        this.stop = stop;
+    }
+
+}
