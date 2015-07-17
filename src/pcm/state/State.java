d077a2707aceb3f334815c53564ca84fde013053
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2015-07-18 01:07:38 +0200

    Fixed: Action sets are different from data sets in that action sets are going to be cleared when another script is loaded - I hope that now its much closer (I don't think identically) to the way PCM handled it

--- src/pcm/state/State.java
+++ src/pcm/state/State.java
@@ -3,7 +3,9 @@ package pcm.state;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 import pcm.model.Action;
@@ -25,6 +27,9 @@ import teaselib.TeaseLib;
  * 
  * set() and unset() actually set the value to either 1 or 0, whereas
  * resetRange() removes the value from the storage.
+ *
+ * TODO add full test coverage as this central class responsible for the flow of
+ * execution is completely untested
  * 
  * * @author someone
  *
@@ -35,7 +40,24 @@ public class State {
     private final String root;
 
     private Script script = null;
+
+    /**
+     * Actions are set separately, they're reset each time a script is loaded.
+     */
+    private Set<Integer> actions = new HashSet<Integer>();
+
+    /**
+     * Data is never cleared when loading a script, but they're all cleared when
+     * the startup script is run.
+     * 
+     * Instead, at script start the save range is restored, followed by
+     * additional .resetrange commands.
+     */
     private Map<Integer, Integer> data = new HashMap<Integer, Integer>();
+
+    /**
+     * Action may also contain a time stamp.
+     */
     private Map<Integer, Date> times = new HashMap<Integer, Date>();
 
     private int step = 0;
@@ -59,8 +81,9 @@ public class State {
     public void restore(Script script) {
         this.script = script;
         step = 0;
+        actions.clear();
         action2StepMap.clear();
-        // Restore restores
+        // Restore restores:
         // - nothing if nothing has been saved before
         // - The range last saved
         String start = read("save.start");
@@ -108,9 +131,11 @@ public class State {
     public void save(ActionRange range) {
         write("save.start", range.start);
         write("save.end", range.end);
+        // Data values
         for (Integer i : range) {
             Integer value = get(i);
-            write(i.toString(), value.equals(UNSET) ? null : value);
+            final boolean valueUnset = value.equals(UNSET);
+            write(i.toString(), valueUnset ? null : value);
         }
         // time values
         StringBuilder keys = null;
@@ -147,6 +172,8 @@ public class State {
     public Integer get(Integer n) {
         if (data.containsKey(n)) {
             return data.get(n);
+        } else if (actions.contains(n)) {
+            return SET;
         } else {
             return UNSET;
         }
@@ -154,6 +181,7 @@ public class State {
 
     public void set(Integer n) {
         data.put(n, SET);
+        actions.remove(n);
         times.remove(n);
     }
 
@@ -183,6 +211,7 @@ public class State {
     }
 
     public void unset(Integer n) {
+        actions.remove(n);
         data.remove(n);
         times.remove(n);
     }
@@ -197,18 +226,21 @@ public class State {
     public void repeatSet(Integer n, int m) {
         Integer v = -m;
         data.put(n, v.equals(SET) ? SET : v);
+        actions.remove(n);
         times.remove(n);
     }
 
     public void repeatAdd(Integer n, int m) {
         Integer v = data.containsKey(n) ? data.get(n) - m : -m;
         data.put(n, v.equals(SET) ? SET : v);
+        actions.remove(n);
         times.remove(n);
     }
 
     public void repeatDel(Integer n, int m) {
         int v = data.containsKey(n) ? data.get(n) + m : 0;
         data.put(n, v < SET ? new Integer(v) : SET);
+        actions.remove(n);
         times.remove(n);
     }
 
@@ -218,15 +250,29 @@ public class State {
 
     public void set(Action action) throws ScriptExecutionError {
         Integer n = action.number;
-        if (get(n).equals(SET)) {
+        final Integer value = get(n);
+        if (value.equals(SET)) {
             throw new ScriptExecutionError("Action already set");
+        } else if (value.equals(UNSET)) {
+            // advance from UNSET to SET
+            // Action sets are not saved, and unlike data sets cleared on
+            // restore
+            actions.add(n);
+            data.remove(n);
+            times.remove(n);
+            rememberActionStep(n);
         } else {
-            set(n);
-            if (!action2StepMap.containsKey(n)) {
-                action2StepMap.put(n, new Integer(step));
-            }
-            step++;
+            // count up from n to UNSET == 0
+            repeatDel(n, 1);
+            rememberActionStep(n);
         }
+    }
+
+    private void rememberActionStep(Integer n) {
+        if (!action2StepMap.containsKey(n)) {
+            action2StepMap.put(n, new Integer(step));
+        }
+        step++;
     }
 
     public int getStep() {
