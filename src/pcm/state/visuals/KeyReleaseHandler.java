bce38863f9a28086f137d56a90d424d0a659a114
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2016-10-25 13:10:10 +0200

    Fxied: commands are issued too early because they are executed immediately instead during the next render call - waiting until the previous set has completed all

--- src/pcm/state/visuals/KeyReleaseHandler.java
+++ src/pcm/state/visuals/KeyReleaseHandler.java
@@ -5,6 +5,9 @@ package pcm.state.visuals;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import pcm.controller.Player;
 import pcm.state.Visual;
 import teaselib.core.devices.DeviceCache;
@@ -17,6 +20,8 @@ import teaselib.core.media.MediaRendererThread;
  *
  */
 public class KeyReleaseHandler implements Visual {
+    private static final Logger logger = LoggerFactory
+            .getLogger(KeyReleaseHandler.class);
     public static final String Prepare = "prepare";
     public static final String Start = "start";
     public static final String Sleep = "sleep";
@@ -39,17 +44,21 @@ public class KeyReleaseHandler implements Visual {
 
     @Override
     public void render(final Player player) {
+        player.completeAll();
+        logger.info(
+                command + (durationMinutes > 0 ? " " + durationMinutes : ""));
         if (command.equalsIgnoreCase(Prepare)) {
             connectToDeviceAndArm(player);
-        } else if (player.keyRelease != null
-                && player.keyReleaseActuator >= 0) {
-            if (command.equalsIgnoreCase(Start)) {
-                player.keyRelease.start(player.keyReleaseActuator,
-                        durationMinutes);
-            } else if (command.equalsIgnoreCase(Sleep)) {
-                player.keyRelease.sleep(durationMinutes);
-            } else if (command.equalsIgnoreCase(Release)) {
-                player.keyRelease.release(player.keyReleaseActuator);
+        } else {
+            if (player.keyRelease != null && player.keyReleaseActuator >= 0) {
+                if (command.equalsIgnoreCase(Start)) {
+                    player.keyRelease.start(player.keyReleaseActuator,
+                            durationMinutes);
+                } else if (command.equalsIgnoreCase(Sleep)) {
+                    player.keyRelease.sleep(durationMinutes);
+                } else if (command.equalsIgnoreCase(Release)) {
+                    player.keyRelease.release(player.keyReleaseActuator);
+                }
             }
         }
     }
@@ -57,13 +66,11 @@ public class KeyReleaseHandler implements Visual {
     private void connectToDeviceAndArm(final Player player) {
         player.keyRelease = null;
         player.keyReleaseActuator = -1;
-        MediaRenderer keyReleaseArm = new MediaRendererThread(
-                player.teaseLib) {
+        MediaRenderer keyReleaseArm = new MediaRendererThread(player.teaseLib) {
             @Override
             protected void renderMedia()
                     throws InterruptedException, IOException {
-                KeyRelease keyRelease = KeyRelease.Devices
-                        .getDefaultDevice();
+                KeyRelease keyRelease = KeyRelease.Devices.getDefaultDevice();
                 startCompleted();
                 mandatoryCompleted();
                 allCompleted();
