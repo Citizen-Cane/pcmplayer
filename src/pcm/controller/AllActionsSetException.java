48ad7203ad6135b50f43d3f2a53bbb53ad4b0684
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2017-01-26 22:09:44 +0100

    Improved error reporting for AllActionsSet

--- src/pcm/controller/AllActionsSetException.java
+++ src/pcm/controller/AllActionsSetException.java
@@ -1,15 +1,21 @@
 package pcm.controller;
 
+import java.util.List;
+
 import pcm.model.Action;
+import pcm.model.ActionRange;
 import pcm.model.Script;
 import pcm.model.ScriptExecutionException;
 
 public class AllActionsSetException extends ScriptExecutionException {
-
     private static final long serialVersionUID = 1L;
 
-    public AllActionsSetException(Action action, Script script) {
-        super(action != null ? "All actions set: " + action.number
-                : "All actions set in script section", script);
+    public final List<Action> actions;
+
+    public AllActionsSetException(List<Action> actions, ActionRange actionRange,
+            Script script) {
+        super((actions.size() > 1 ? "All actions set: " : "Empty range ")
+                + actionRange.toString(), script);
+        this.actions = actions;
     }
 }
