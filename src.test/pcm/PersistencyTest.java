b05394008c732b647632d4a2857c3167b6649f4c
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2017-01-26 22:26:00 +0100

    Updated script persistency tests because the recent .resetrange optimization allows the test to fail a little later

--- src.test/pcm/PersistencyTest.java
+++ src.test/pcm/PersistencyTest.java
@@ -1,9 +1,11 @@
 package pcm;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import pcm.controller.AllActionsSetException;
 import pcm.controller.Player;
 import pcm.model.ActionRange;
 import pcm.state.MappedState;
@@ -14,6 +16,7 @@ import teaselib.Toys;
 public class PersistencyTest {
 
     @Test
+    // TODO Throws AllActionsSet - expected test result
     public void testThatResetRangeUnsetsMappings() throws Exception {
         Player player = TestUtils.createPlayer(getClass());
 
@@ -28,15 +31,23 @@ public class PersistencyTest {
         player.loadScript(
                 "PersistencyTest_ResetRangeUnsetsMappings_MainScript");
 
-        // these assertions indicate that loading the script has deleted
-        // the toy that has been mapped to action 365
-        // TODO should be 0 and UNSET, but I've changed initializing
-        assertEquals(0, player.items(Toys.Collars).available().size());
-        assertEquals(State.UNSET, player.state.get(365));
+        // Correct because the optimized .resetrange only clears previously
+        // restored items,
+        // and thus won't clear the toy anymore
+        assertEquals(1, player.items(Toys.Collars).available().size());
+        assertEquals(State.SET, player.state.get(365));
 
         player.state.unset(100);
         player.state.set(101);
-        pcm.util.TestUtils.play(player, new ActionRange(1000), null);
+        try {
+            // This throws since when returning from the subscript,
+            // action 365 has been set, and is thus cleared by .resetrange
+            pcm.util.TestUtils.play(player, new ActionRange(1000), null);
+            assertTrue(false);
+        } catch (AllActionsSetException e) {
+            assertEquals(1, e.actions.size());
+            assertEquals(1001, e.actions.get(0).number);
+        }
     }
 
     @Test
