5cc9b190211df1a2608305c20c48ffc925ed003f
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2018-03-20 08:45:16 +0100

    Added symbols for *f|tv relationships

--- src/pcm/model/Symbols.java
+++ src/pcm/model/Symbols.java
@@ -5,8 +5,11 @@ import java.util.HashMap;
 public class Symbols extends HashMap<String, String> {
     private static final long serialVersionUID = 1L;
 
+    public static final String Mm = "Mm";
     public static final String Fm = "Fm";
+    public static final String Mf = "Mf";
     public static final String Ff = "Ff";
+    public static final String Mtv = "Mtv";
     public static final String Ftv = "Ftv";
 
     public Symbols() {
@@ -15,8 +18,11 @@ public class Symbols extends HashMap<String, String> {
 
     public static Symbols getDominantSubmissiveRelations() {
         Symbols symbols = new Symbols();
+        symbols.put(Mm, Mm);
         symbols.put(Fm, Fm);
+        symbols.put(Mf, Mf);
         symbols.put(Ff, Ff);
+        symbols.put(Mtv, Mtv);
         symbols.put(Ftv, Ftv);
         return symbols;
     }
