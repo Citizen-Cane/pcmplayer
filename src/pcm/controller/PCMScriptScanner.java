d9568f8666407ceb25e75872471c0821c5fbfd00
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2018-12-11 22:33:18 +0100

    Fixed regression: speech recording was broken

--- src/pcm/controller/PCMScriptScanner.java
+++ src/pcm/controller/PCMScriptScanner.java
@@ -3,9 +3,7 @@ package pcm.controller;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
-import pcm.model.AbstractAction.Statement;
 import pcm.model.Action;
 import pcm.model.Script;
 import pcm.state.Visual;
@@ -29,19 +27,14 @@ public class PCMScriptScanner implements ScriptScanner {
     public Iterator<Message> iterator() {
         List<Message> r = new ArrayList<>();
         for (Action action : script.actions.values()) {
-            Map<Statement, Visual> visuals = action.visuals;
-            if (visuals != null) {
-                if (visuals.containsKey(Statement.Message)) {
-                    Visual visual = visuals.get(Statement.Message);
-                    if (visual instanceof pcm.state.visuals.SpokenMessage) {
-                        pcm.state.visuals.SpokenMessage spokenMessage = (pcm.state.visuals.SpokenMessage) visual;
-                        for (Message message : spokenMessage.getMessages()) {
-                            r.add(message);
-                        }
-                    } else {
-                        // It's NoMessage, and there's nothing to do
-                    }
+            Visual visual = action.message;
+            if (visual instanceof pcm.state.visuals.SpokenMessage) {
+                pcm.state.visuals.SpokenMessage spokenMessage = (pcm.state.visuals.SpokenMessage) visual;
+                for (Message message : spokenMessage.getMessages()) {
+                    r.add(message);
                 }
+            } else {
+                // It's NoMessage, and there's nothing to do
             }
         }
         return r.iterator();
